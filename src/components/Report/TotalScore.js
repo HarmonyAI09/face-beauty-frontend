3896cd1b9056b50e970cd5ddaba82bcfab76cf2d
crazyGuru <alimanson0412@gmail.com>
2024-02-22 11:23:04 +0300

    fix the total score to fixed 2

--- src/components/Report/TotalScore.js
+++ src/components/Report/TotalScore.js
@@ -6,7 +6,7 @@ const TotalProfileScore = () => {
 
   return (
     <div className="total_score_view">
-      { oneProfile.percentage }% FACIAL HARMONY
+      { oneProfile.percentage.toFixed(2) }% FACIAL HARMONY
     </div>
   )
 };
